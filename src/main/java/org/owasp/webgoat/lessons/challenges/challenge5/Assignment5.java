a7a0d75345d93e2bafbbf8a7421e21d3adb6f55c
eLNiTa <e.arcaz.2022@alumnos.urjc.es>
2026-02-25 12:44:23 +0100

    Correcting SQLi vulnerability in file Assignment5.java

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -24,6 +24,8 @@ package org.owasp.webgoat.lessons.challenges.challenge5;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.owasp.webgoat.container.LessonDataSource;
@@ -47,28 +49,29 @@ public class Assignment5 extends AssignmentEndpoint {
   @PostMapping("/challenge/5")
   @ResponseBody
   public AttackResult login(
-      @RequestParam String username_login, @RequestParam String password_login) throws Exception {
-    if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
-      return failed(this).feedback("required4").build();
-    }
-    if (!"Larry".equals(username_login)) {
-      return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
-    }
-    try (var connection = dataSource.getConnection()) {
-      PreparedStatement statement =
-          connection.prepareStatement(
-              "select password from challenge_users where userid = '"
-                  + username_login
-                  + "' and password = '"
-                  + password_login
-                  + "'");
-      ResultSet resultSet = statement.executeQuery();
+      @RequestParam String username_login, @RequestParam String password_login) throws SQLException {
 
-      if (resultSet.next()) {
-        return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
-      } else {
-        return failed(this).feedback("challenge.close").build();
+      if (!StringUtils.hasText(username_login) || !StringUtils.hasText(password_login)) {
+          return failed(this).feedback("required4").build();
       }
-    }
+      if (!"Larry".equals(username_login)) {
+          return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
+      }
+
+      try (var connection = dataSource.getConnection()) {
+          String query = "SELECT password FROM challenge_users WHERE userid = ? AND password = ?";
+          try (PreparedStatement statement = connection.prepareStatement(query)) {
+              statement.setString(1, username_login);
+              statement.setString(2, password_login);
+
+              ResultSet resultSet = statement.executeQuery();
+
+              if (resultSet.next()) {
+                  return success(this).feedback("challenge.solved").feedbackArgs(flags.getFlag(5)).build();
+              } else {
+                  return failed(this).feedback("challenge.close").build();
+              }
+          }
+      }
   }
 }
